5d12602d03f98a52e3b9ea0ef35a909180035f1f
Anh Nguyen <anh.ns.dev@gmail.com>
2021-08-24 17:52:17 +0700

    Added menu

---
+++ Assets/Scrips/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadMenuScene() {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGameScene() {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
