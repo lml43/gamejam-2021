3c43ab8286a459cd65bbcf31351f91bbfc6cb98e
manhlinh0403 <manhlinh0403@gmail.com>
2021-08-29 02:15:17 +0200

    boss angry

--- Assets/Scrips/Boss.cs
+++ Assets/Scrips/Boss.cs
@@ -13,16 +13,25 @@ public class Boss : MonoBehaviour
   public float maxChaseTime = 5f;
   public float timeToCharge = 2f;
 
+  [Header("Chasing")]
+  public Transform[] targets;
+  public Vector2 minPosition;
+  public Vector2 maxPosition;
+
   private Rigidbody2D body;
   private Vector2 movementToPlayer;
   private Vector2 movementToSpot;
+  private Animator anim;
+  private Transform target;
 
   private int randomSpot;
 
   private float chaseTime = 0;
 
   void Start() {
+    target = player;
     body = this.GetComponent<Rigidbody2D>();
+    anim = this.GetComponent<Animator>();
 
     randomSpot = Random.Range(0, moveSpots.Length);
   }
@@ -50,16 +59,34 @@ public class Boss : MonoBehaviour
     if (chaseTime > 0) {
       chaseTime -= Time.deltaTime;
     }
+
+    ChangeAnim(directionToSpot);
   }
 
   private void FixedUpdate() {
-    if (chaseTime <= 0) {
-      MoveToSpot(movementToSpot);
-    } else if (chaseTime < timeToCharge) {
-      ChargePlayer(movementToPlayer);
-    } else {
-      ChasePlayer(movementToPlayer);
+
+    if (anim.GetBool("chasing") && isOutOfBound()) {
+      anim.SetBool("chasing", false);
+      anim.SetBool("isAngry", false);
+      body.velocity = Vector2.zero;
+    } else if (!anim.GetBool("isAngry")) {
+      if (chaseTime <= 0) {
+        MoveToSpot(movementToSpot);
+      } else if (chaseTime < timeToCharge) {
+        ChargePlayer(movementToPlayer);
+      } else {
+        ChasePlayer(movementToPlayer);
+      }
     }
+
+    
+  }
+
+  private bool isOutOfBound() {
+    return transform.position.x < minPosition.x 
+      || transform.position.x > maxPosition.x 
+      || transform.position.y < minPosition.y 
+      || transform.position.y > maxPosition.y;
   }
 
   int GetNewSpot(int currentSpot) {
@@ -84,10 +111,47 @@ public class Boss : MonoBehaviour
 
   public void TakeDamage(float damage) {
     maxHealth -= damage;
-    chaseTime = maxChaseTime;
+    // chaseTime = maxChaseTime;
+
+    StartCoroutine(AngryCo());
 
     if (maxHealth <= 0) {
+      // TODO: Endgame
       this.gameObject.SetActive(false);
     }
+  }
+
+  private void ChangeAnim(Vector2 direction) {
+    if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y)) {
+        if (direction.x > 0) {
+            SetAnimVector(Vector2.right);
+        } else if (direction.x < 0) {
+            SetAnimVector(Vector2.left);
+        }
+    } else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y)) {
+        if (direction.y > 0) {
+            SetAnimVector(Vector2.up);
+        } else if (direction.y < 0) {
+            SetAnimVector(Vector2.down);
+        }
+    }
+  }
+
+  private void SetAnimVector(Vector2 vector) {
+      anim.SetFloat("moveX", vector.x);
+      anim.SetFloat("moveY", vector.y);
+  }
+
+  private IEnumerator AngryCo() {
+    anim.SetBool("isAngry", true);
+    yield return new WaitForSeconds(1.2f);
+    Attack(target);
+  }
+
+  private void Attack(Transform target) {
+    anim.SetBool("chasing", true);
+    Vector2 difference = target.position - transform.position;
+    difference = difference.normalized * chargeSpeed;
+    body.AddForce(difference, ForceMode2D.Impulse);
   }
 }
